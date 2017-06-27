ba6d5fddc8fcc2c4af4d12741405f3200e88c602
java2019 <java_2019@mail.ru>
2017-06-27 12:57:43 +0300

    test1

--- app/src/main/java/com/samples/network/webkit/MainActivity.java
+++ app/src/main/java/com/samples/network/webkit/MainActivity.java
@@ -1,13 +1,22 @@
 package com.samples.network.webkit;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.browser);
+        webView.getSettings().setJavaScriptEnabled(true);
+        String uri = "https://www.google.ru/";
+        webView.loadUrl(uri);
     }
 }
